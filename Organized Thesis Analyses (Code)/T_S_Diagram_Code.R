55890112773f8704629e1a5259de195e0b4334e7
varyl97 <86377967+varyl97@users.noreply.github.com>
2021-12-01 19:31:28 -0800

    Change colors

--- Organized Thesis Analyses (Code)/T_S_Diagram_Code.R
+++ Organized Thesis Analyses (Code)/T_S_Diagram_Code.R
@@ -1,4 +1,3 @@
-
 # T-S Diagrams with Species Overlaid --------------------------------------
 #This code creates a temperature-salinity diagram (T-S Diagram) for the Eastern Bering Sea and overlays 
 #   polygons that represent conditions that are associated with each individual species. 
@@ -25,7 +24,6 @@ sal<-seq(from=mins,to=maxs,length.out=180)
 sigma.c<-outer(sal,temp,FUN=function(S,t)sw_dens(S=S,t=t)-1000)
 sigma.c
 
-apcol<-adjustcolor('#93499F',alpha.f=0.6)
 pkcol<-adjustcolor('#BFA0C6',alpha.f=0.6)
 
 
@@ -33,12 +31,13 @@ windows()
 par(mai=c(1,1,0.5,0.9))
 contour2D(x=sal,y=temp,z=sigma.c,lwd=2,main='Temperature-Salinity Diagram',col='black',
           xlab=expression('Salinity (psu)'),ylab=expression('Temperature'*~degree*C*')'))
-DrawEllipse(x=31.5,y=6.35,radius.x=0.5,radius.y=1.15,rot=0, border='grey69',col='#410E48')#x axis = salinity, y axis = temperature
-DrawEllipse(x=31.4,y=5,radius.x=0.4,radius.y=0.5,rot=0,border='grey69',col=apcol)
+DrawEllipse(x=31.5,y=6.35,radius.x=0.5,radius.y=1.15,rot=0, border='grey69',col='coral')#x axis = salinity, y axis = temperature
+DrawEllipse(x=31.4,y=5,radius.x=0.4,radius.y=0.5,rot=0,border='grey69',col='cyan4')
 DrawEllipse(x=32,y=5,radius.x=1,radius.y=1,rot=0,border='grey69',col=pkcol)
-DrawEllipse(x=30.75,y=9.75,radius.x=0.75,radius.y=0.75,border='grey69',col='#EBE6EC')
-DrawEllipse(x=31.75,y=2.5,radius.x=0.75,radius.y=2,border='grey69',col='#CFECCF')
-DrawEllipse(x=31,y=6.625,radius.x=0.75,radius.y=0.875,border='grey69',col='#7CBF7C')
+DrawEllipse(x=30.75,y=9.75,radius.x=0.75,radius.y=0.75,border='grey69',col='blue4')
+DrawEllipse(x=31.75,y=2.5,radius.x=0.75,radius.y=2,border='grey69',col='darkgoldenrod4')
+DrawEllipse(x=31,y=6.625,radius.x=0.75,radius.y=0.875,border='grey69',col='darkorchid4')
 legend('bottomleft',legend=c('Flathead Sole','Alaska Plaice','Walleye Pollock','Yellowfin Sole',
-                              'Northern Rock Sole','Pacific Cod'),col=c('#410E48','#93499F','#BFA0C6','#EBE6EC','#CFECCF','#7CBF7C'),
+                              'Northern Rock Sole','Pacific Cod'),col=c('coral','cyan4','#BFA0C6','blue4',
+                                                                        'darkgoldenrod4','darkorchid4'),
        bg='snow1',pch=16)
